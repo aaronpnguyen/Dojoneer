2cafb5278461d4b53788eb1b4afeceae333a38ad
Amiculay <aaronnguyen1123@gmail.com>
2022-07-26 00:00:23 -0400

    Added state

--- client/src/components/Notepad.jsx
+++ client/src/components/Notepad.jsx
@@ -1,22 +1,20 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 const Notepad = () => {
-
+    let [info, setInfo] = useState("Thank you for visiting Dojoneer! This notepad will clear when the browser is closed..")
     const [show, setShow] = useState(true);
+    
     return (
         <div className="notepad">
             <div>
                 <button className='open-btn notes' onClick={() => setShow(!show)}> Notes </button>
             </div>
-
-
             {
                 show &&
                 <div className="paper-content">
-                    <textarea defaultValue="Thank you for visiting Dojoneer! This notepad will clear when the browser is closed.."></textarea>
+                    <textarea onChange={e => setInfo(e.target.value)}>{info}</textarea>
                 </div>
             }
-
         </div>
     );
 }
