61b0f19166cc9c0a18b4519421c9876da3b7b88d
Amiculay <aaronnguyen1123@gmail.com>
2022-07-26 04:40:43 -0400

    Added tasks

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
-import {Routes, Route} from 'react-router-dom'
 import './App.css';
 import Notepad from './components/Notepad';
+import Tasks from './components/Tasks'
 
 
 function App() {
   return (
     <div className="App container">
       <h1 className='logo'>Dojoneer</h1>
-
-      <Notepad />
+      <Tasks/>
+      <Notepad/>
 
     </div>
   );
