eec16edd30fac51d2b40c8cc5eefa8c8a23b2eda
Amiculay <aaronnguyen1123@gmail.com>
2022-07-27 17:26:30 -0400

    Added toggle features, fixed input timer

--- client/src/components/Timer.jsx
+++ client/src/components/Timer.jsx
@@ -2,11 +2,20 @@ import React, {useEffect, useState, useRef} from "react";
 
 function Timer() {
     let [inputTime, setInputTime] = useState('');
-    let [seconds, setSeconds] = useState();
-    let [toggle, setToggle] = useState(false)
+    let [seconds, setSeconds] = useState(inputTime);
+    let [toggleStart, setToggleStart] = useState(false);
+    let [toggleResume, setToggleResume] = useState(false)
 
     const renders = useRef(inputTime);
     const timerId = useRef();
+
+    useEffect(() => {
+        if (seconds === 0) {
+            resetTimer()
+        }
+        // fuck u stupid error (ง︡'-'︠)ง
+        // eslint-disable-next-line
+    }, [seconds])
 
     const handleChange = (e) => {
         setInputTime(e.target.value);
@@ -14,17 +23,30 @@ function Timer() {
     }
 
     const startTimer = () => {
+        if (inputTime) {
+            setSeconds(inputTime)
+            timerId.current = setInterval(() => {
+                renders.current++;
+                setSeconds(inputTime => inputTime - 1);
+            }, 1000)
+            setToggleStart(true)
+            setInputTime("")
+        }
+    }
+
+    const resumeTimer = () => {
+        setSeconds(seconds)
         timerId.current = setInterval(() => {
             renders.current++;
-            setSeconds(prev => prev - 1);
+            setSeconds(seconds => seconds - 1);
         }, 1000)
-        setToggle(true)
+        setToggleResume(true)
     }
 
     const stopTimer = () => {
         clearInterval(timerId.current);
         timerId.current = inputTime;
-        setToggle(false)
+        setToggleResume(true)
     }
 
     const resetTimer = () => {
@@ -33,19 +55,24 @@ function Timer() {
             renders.current--;
             setSeconds(inputTime);
         }
-        setToggle(false)
+        setToggleStart(false)
+        setToggleResume(false)
     }
 
     return(
         <>
             <input type="int" value={inputTime} placeholder="Set Timer" onChange={handleChange} />
             <section>
-                <button disabled={toggle} onClick={startTimer}>Start</button>
+                {!toggleStart?
+                    <button onClick={startTimer}>Start</button>:
+                    <button disabled={!toggleResume} onClick={resumeTimer}>Resume</button>
+                }
                 <button onClick={stopTimer}>Stop</button>
                 <button onClick={resetTimer}>Reset</button>
             </section>
-            <p style={{color: "white"}}>Seconds: {seconds}</p>
-            <p style={{color: "white"}}>{inputTime}</p>
+            {
+                <p style={{color: "white"}}>Seconds: {seconds}</p>
+            }
         </>
     )
 }
