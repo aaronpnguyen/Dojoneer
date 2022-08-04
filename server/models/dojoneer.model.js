5d1465bcf02ca094a397b966cf293dd5f2871d15
Amiculay <aaronnguyen1123@gmail.com>
2022-08-04 00:07:24 -0400

    Deleted Server

--- server/models/dojoneer.model.js
+++
@@ -1,1 +0,0 @@
-dojoneer.model.js
--- server/routes/dojoneer.routes.js
+++
@@ -1,9 +0,0 @@
-const DojoneerController = require('../controllers/dojoneer.controller');
-
-module.exports = app => {
-	app.get('/api/NAMES', DojoneerController.findAllNAMES);
-	app.post('/api/NAMES', DojoneerController.createNewNAME);
-	app.get('/api/NAMES/:_id', DojoneerController.findOneNAME);
-	app.put('/api/NAMES/:_id', DojoneerController.updateOneNAME);
-	app.delete('/api/NAMES/:_id', DojoneerController.deleteNAME);
-}
